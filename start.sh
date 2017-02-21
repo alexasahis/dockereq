8db5022ea0dc7de6c85d79b97cf1d4b43d9f104a
Xackery Xtal <xackery@gmail.com>
2017-02-21 14:42:12 -0800

    added check for world

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+if [ -f "eqemu_data/world" ]; then
+else
+	echo "Could not find world inside eqemu_data, please copy these binaries or run build.sh"
+	exit
+fi
+
 docker-compose down
 docker-compose up
 echo "Running Shared Memory on eqemu..."
